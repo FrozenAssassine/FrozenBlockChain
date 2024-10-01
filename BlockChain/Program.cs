57db6247f3db92d9a44853005b93a9f0dfc0bb63
Julius Kirsch <Julius@skirsch.de>
2024-10-01 22:32:04 +0200

    feat: transaction changed

--- BlockChain/Program.cs
+++ BlockChain/Program.cs
@@ -5,18 +5,18 @@ class Program
     static void Main(string[] args)
     {
         Blockchain blockchain = new Blockchain();
-
-        blockchain.CreateTransaction(new Transaction("Julius", "Alice", 5.0));
-        blockchain.CreateTransaction(new Transaction("Alice", "Bob", 2.0));
-
         Console.WriteLine("Mining...");
 
-        for (int i = 0; i<100; i++)
+        for (int i = 0; i < 5; i++)
         {
             blockchain.MinePendingTransactions();
-
-            Console.WriteLine("Balance of Julius: " + blockchain.GetBalance("Julius"));
         }
+        
+        blockchain.CreateTransaction(new Transaction("Julius", "Alice", 5.0));
+
+        Console.WriteLine("Balance of Julius: " + blockchain.GetBalance("Julius"));
+
+
         Console.WriteLine("Balance of Alice: " + blockchain.GetBalance("Alice"));
         Console.WriteLine("Balance of Bob: " + blockchain.GetBalance("Bob"));
 
